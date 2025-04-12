1605c21cb2e886378db2b7a4fa7e3a99fd9cf6e4
artursousac <artursousac@hotmail.com>
2025-04-12 19:10:40 -0300

    .

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -7,18 +7,19 @@ destino = "quixada-ce"
 # Data deve ser no formato dd-m-yyy
 dataViagem = "18-4-2025"
 
+# URL contendo os parametros necessários do viaje-guanabara e realizando a requisição http
 url = f"https://viajeguanabara.com.br/onibus/{origem}/{destino}?departureDate={dataViagem}&passengers=1:1"
 response = requests.get(url)
 
-# 2. Verificar se deu certo
+# Se a response houver sucesso, analisa o HTML com o BeatifulSoup e extrai os dados necessários.
 if response.status_code == 200:
     html = response.text
 
-    # 3. Analisar o HTML
     soup = BeautifulSoup(html, 'html.parser')
 
-    # 4. Extrair os dados (exemplo: o primeiro parágrafo)
     paragrafo = soup.find('p')
     print(paragrafo.text)
+
+# Se não tiver sucesso, informa que houve erro.
 else:
     print("Erro ao acessar a página:", response.status_code)
