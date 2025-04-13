2d985b6cd1783f79942aa31caa86639ad481ea1a
artursousac <artursousac@hotmail.com>
2025-04-13 16:09:51 -0300

    .

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,15 +1,9 @@
 from viajeguanabara import procurar_passagem
 from telegram import enviar_telegram, enviar_telegram_unico
 from datetime import datetime
-from dotenv import load_dotenv
-import os
-load_dotenv()
-token_telegram = os.getenv("TOKEN_TELEGRAM")
-chat_id_telegram_1 = os.getenv("CHAT_ID_1")
 
 procurar_passagem("fortaleza-ce", "quixada-ce", "18-4-2025")
 procurar_passagem("quixada-ce", "fortaleza-ce", "21-4-2025")
-enviar_telegram_unico("Teste", token_telegram, chat_id_telegram_1)
 
 try:
     caminho_log = 'Log.txt'
--- Backend/src/telegram.py
+++ Backend/src/telegram.py
@@ -15,15 +15,15 @@ def enviar_telegram(mensagem, token, chat_id_1, chat_id_2):
     except Exception as e:
         print(f"Erro ao enviar a mensagem: {e}")
 
-def enviar_telegram_unico(mensagem, token, chat_id_1):
+def enviar_telegram_unico(mensagem, token, chat_id):
     message = mensagem
-    url_1 = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id_1}&text={message}"
+    url = f"https://api.telegram.org/bot{token}/sendMessage?chat_id={chat_id}&text={message}"
 
     try:
-        response_1 = requests.get(url_1)
-        if response_1.status_code == 200:
+        response = requests.get(url)
+        if response.status_code == 200:
             print("Mensagem enviada com sucesso!")
         else:
-            print(f"Falha ao enviar a mensagem: {response_1}")
+            print(f"Falha ao enviar a mensagem: {response}")
     except Exception as e:
         print(f"Erro ao enviar a mensagem: {e}")
