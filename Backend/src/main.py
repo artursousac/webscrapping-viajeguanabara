b454aefd0fe96daa4811b83c47fd0251e4c56248
artursousac <artursousac@hotmail.com>
2025-04-13 15:25:30 -0300

    .

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -3,11 +3,12 @@ from telegram import enviar_telegram
 from datetime import datetime
 import os
 
-caminho_log = os.path.join(os.path.dirname(__file__), '..', 'dist', 'Log.txt')
-with open(caminho_log, 'a') as log:
-    log.write(f'Nova execucao: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n')
-
 procurar_passagem("fortaleza-ce", "quixada-ce", "18-4-2025")
 procurar_passagem("quixada-ce", "fortaleza-ce", "21-4-2025")
 
-
+try:
+    caminho_log = 'Log.txt'
+    with open(caminho_log, 'a') as log:
+        log.write(f'Nova execucao: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}\n')
+except:
+    print("Erro")
